5fe9a02e028bfb98d60d8c90d38a8df6224fca3f
jshepp27 <36790461+jshepp27@users.noreply.github.com>
2023-05-07 16:07:59 +0100

    upgraded Agent with experience replay; scaled rewards and state through min-max normalisation

--- agent.py
+++ agent.py
@@ -4,8 +4,10 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch as T
 import numpy as np
-import environment
 import matplotlib.pyplot as plt
+
+import environment
+import random
 
 np.set_printoptions(precision=8, suppress=True)
 
@@ -15,20 +17,25 @@ N_ACTIONS = 3
 INPUT_DIMS = 6
 EPSILON = 1.00
 GAMMA = 0.09
-DECAY = 0.03
+DECAY = 0.001
 EPS_MIN = 0.01
+BUFFER_SIZE = 200
+BATCH_SIZE = 64
 
 """
 TODOs:
 - Agent Evaluation
--- Average over Multiple Episodes
+-- Average over Multiple Episodes 
+- Draw-Model Matrix Dimensions (exp. GPT Output) [DONE]
 
 - Store Agent Model
 - Scale state, reward, target values to work with Network
 - Implement Sigmoid Activation
 - Implement Experience Replay
+- Argparse to parameterize training and eval.
  
 """
+
 class DQN(nn.Module):
     def __init__(self, lr=LEARNING_RATE, n_actions=N_ACTIONS, input_dims=INPUT_DIMS):
         super(DQN, self).__init__()
@@ -49,6 +56,31 @@ class DQN(nn.Module):
 
         return actions
 
+# TODO: Consider 'game-over' state
+class ReplayBuffer:
+    def __init__(self, buffer_size=100):
+        self.buffer = []
+        self.buffer_size = buffer_size
+
+    def add(self, transition):
+        experience = transition
+        # MANAGE BUFFER AS A QUEUE FIFO
+        if len(self.buffer) >= self.buffer_size:
+            self.buffer.pop(0)
+
+        self.buffer.append(experience)
+    def sample(self, batch_size=64):
+        sampled_experiences = random.sample(self.buffer, batch_size)
+        states, actions, rewards, states_ = [], [], [], []
+
+        for exp in sampled_experiences:
+            state, action, reward, state_ = exp
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+            states_.append(state)
+
+        return states, actions, rewards, states_
 class Agent():
     def __init__(self,
                  input_dims=INPUT_DIMS,
@@ -57,11 +89,18 @@ class Agent():
                  gamma=GAMMA,
                  epsilon=EPSILON,
                  eps_dec=DECAY,
-                 eps_min=EPS_MIN):
+                 eps_min=EPS_MIN,
+                 buffer_size=BUFFER_SIZE,
+                 batch_size=BATCH_SIZE):
 
         self.lr = lr
         self.input_dims = input_dims
         self.n_actions = n_actions
+
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
+        self.exp_replay = ReplayBuffer(buffer_size=self.buffer_size)
+
         self.gamma = gamma
         self.epsilon = epsilon
         self.eps_dec = eps_dec
@@ -72,12 +111,13 @@ class Agent():
 
     def choose_action(self, observation):
         if np.random.random() > self.epsilon:
+            # EXPLORE
             state = T.tensor(observation, dtype=T.float).to(self.Q.device)
             actions = self.Q.forward(state)
             action = T.argmax(actions)
 
-
         else:
+            # EXPLOIT
             action = np.random.choice(self.action_space)
 
         return action
@@ -85,18 +125,21 @@ class Agent():
     def decrement_epsilon(self):
         self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min else self.eps_min
 
-    def learn(self, state, action, reward, state_):
-        # TODO (?) Clear Optimizer
+    def learn(self):
+        if len(self.exp_replay.buffer) < self.batch_size:
+            return
+
+        state, action, reward, state_ = self.exp_replay.sample(self.batch_size)
+
         self.Q.optimizer.zero_grad()
 
         states = T.tensor(state, dtype=T.float).to(self.Q.device)
-        actions = T.tensor(action).to(self.Q.device)
+        actions_taken = T.tensor(action).to(self.Q.device)
         rewards = T.tensor(reward).to(self.Q.device)
         states_ = T.tensor(state_, dtype=T.float).to(self.Q.device)
 
-        # NOTE: Absolute Q-Values, not Actions. Thus, no Argmax.
-        # TODO (?) [actions] controls correct batch size and shape
-        q_pred = self.Q.forward(states)[actions]
+        actions_tensor = T.tensor(actions_taken, dtype=T.long).to(self.Q.device)
+        q_pred = self.Q.forward(states).gather(1, actions_tensor.unsqueeze(1)).squeeze(1)
 
         q_next = self.Q.forward(states_).max()
 
@@ -134,16 +177,13 @@ if __name__ == "__main__":
     n_games = 500
     print(obs)
 
-    #print(obs.shape)
-    #print(env.action_spaces.n)
-
-    agent = Agent(lr=0.0001, input_dims=obs.shape, n_actions=env.action_spaces.n)
+    agent = Agent(lr=0.0001, input_dims=obs.shape, n_actions=env.action_spaces.n, buffer_size=500, batch_size=64)
     #print(agent.Q)
 
     for _ in range(n_games):
         score = 0
         steps = 0
-        max_steps = 1000
+        max_steps = 250
         done = False
         obs, reward, done, info = env.reset()
 
@@ -153,15 +193,21 @@ if __name__ == "__main__":
             action = agent.choose_action(obs)
             obs_, reward, done, info = env.step(action)
             score += reward
-            agent.learn(obs, action, reward, obs_)
+
+            # Increment Experience
+            agent.exp_replay.add((obs, action, reward, obs_))
+            agent.learn()
+
             obs = obs_
 
             # TODO: Improve Implementation
             if done:
-                if env.unmanaged_failure == False:
-                    reward += 100
-                else:
-                    over = True
+                over = True
+                # TODO: How to pass back Episode Success
+                # if env.unmanaged_failure == False:
+                #     reward += 100
+                # else:
+                #     over = True
 
         scores.append(score)
         avg_score = np.mean(scores)
--- environment.py
+++ environment.py
@@ -19,24 +19,41 @@ Consider:
 - Rendering 
 - Animated Plots
 - Pillow
+- Evaluating on EOL extension, age asset maintained on the Network, Cost-Benefit
 """
 
 failure_rates = {
-    "A": 0.0045,
-    "B": 0.0025,
-    "C": 0.0035
+    "A": 0.010,
+    "B": 0.008,
+    "C": 0.009
 }
 class TransmissionAsset(gym.Env):
-    def __init__(self):
+    def __init__(self,
+                 state_min=0,
+                 state_max=1,
+                 reward_min=-1,
+                 reward_max=1,
+                 family_type="A",
+                 failure_rates=failure_rates):
+
         super(TransmissionAsset, self).__init__()
 
         self.initial_age = 1
         self.unmanaged_failure = False
 
-        self.family_type = "A"
+        self.family_type = family_type
         self.family_type_one_hot = np.zeros(len(failure_rates))
 
         self.failure_rate = failure_rates[self.family_type]
+
+        self.C_fix = 0.5
+        self.C_replace = 0.8
+
+        # STATE-SCALING
+        self.state_min = state_min
+        self.state_max = state_max
+        self.reward_min = reward_min
+        self.reward_max = reward_max
 
         # ACTION SPACE
         self.action_spaces = spaces.Discrete(3)
@@ -50,10 +67,12 @@ class TransmissionAsset(gym.Env):
         })
 
         self.state = {}
+        # TODO: (?) Why Re-shape? Understand Re-shape
         self.state_vector = np.zeros(len(self.observation_space.keys())).reshape(-1, 1)
+
     def probability_failure(self, age):
         return float(1 - math.exp(-self.failure_rate * age))
-    def degrade(self, action=0):
+    def degrade(self, action):
         # CASE: a_t
         if action == 0:
             # DO NOTHING
@@ -61,6 +80,7 @@ class TransmissionAsset(gym.Env):
 
         elif action == 1:
             # FIX
+            # TODO: Parameterise
             age_reduction = self.state["age"] - np.random.uniform(10, 30)
             self.state["age"] = round(max(self.state["age"] - age_reduction, 1))
 
@@ -74,9 +94,9 @@ class TransmissionAsset(gym.Env):
         reward = self.probability_failure(age=self.state["age"])
 
         if action == 1:
-            reward -= 0.5 if self.state["probability_failure"] > 0.5 else 1
+            reward -= self.C_fix if self.state["probability_failure"] > 0.5 else 1
         elif action == 2:
-            reward -= 0.8 if self.state["probability_failure"] > 0.5 else 1
+            reward -= self.C_replace if self.state["probability_failure"] > 0.5 else 1
 
         return reward
 
@@ -85,6 +105,7 @@ class TransmissionAsset(gym.Env):
         self.family_type_one_hot[list(failure_rates.keys()).index(self.family_type)] = 1
         self.state["failure_rate"] = self.failure_rate
         self.state["probability_failure"] = self.probability_failure(self.initial_age)
+        self.state["family_type"] = self.family_type_one_hot
 
         self.state_vector = np.array([self.state["age"],
                                       self.state["failure_rate"],
@@ -96,12 +117,15 @@ class TransmissionAsset(gym.Env):
         reward = 0
         done = False
 
+        # MIN-MAX SCALING
+        self.state_vector = (self.state_vector - self.state_min) / (self.state_max - self.state_min)
+
         return self.state_vector, reward, done, {}
 
     def step(self, action):
         """
         Increment Age of Asset in as a Dynamic Process
-        Compute reward r_t, given a_t
+        Consider Backup Diagram: Compute reward r_t, given a_t
         Compute environment transition from s_t, s_t+1 returning updated state
         """
 
@@ -111,13 +135,15 @@ class TransmissionAsset(gym.Env):
         reward = self.compute_reward(action)
 
         done = False
-        if self.state["probability_failure"] >= 0.95:
-            #print("FAILED AT:", self.state["probability_failure"])
+
+        if self.state["probability_failure"] >= 0.91:
             print("UNMANAGED FAILURE")
             self.unmanaged_failure = True
             done = True
-            reward -= 100
+            penalty = reward - 100
+            reward += (penalty - self.reward_min) / (self.reward_max - self.reward_min)
 
+        # RANDOM FAILURE
         # elif np.random.randint(0,1) > 0.95:
         #     print("RANDOM FAILURE")
         #     done = True
@@ -129,23 +155,31 @@ class TransmissionAsset(gym.Env):
         self.state_vector = np.concatenate(
             (self.family_type_one_hot, [self.state["age"], self.state["failure_rate"], self.state["probability_failure"]]))
 
+        # MIN-MAX SCALING
+        self.state_vector = (self.state_vector - self.state_min) / (self.state_max - self.state_min)
+
+        # Reward scaling
+        reward = (reward - self.reward_min) / (self.reward_max - self.reward_min)
+
         return self.state_vector, reward, done, self.state
     def render(self):
+        # TODO: Render Tabluar Representation
         pass
 
-# env = TransmissionAsset()
-#
-# env.reset()
-# for _ in range(100):
-#     state, reward, done, info = env.step(action=0)
-#     if done:
-#         print("ASSET FAILED")
-#         print(info["probability_failure"])
-#         break
-#
-#     else:
-#         print(info)
-#         print(state)
-#         print(reward, done)
-#         print("")
+
+env = TransmissionAsset()
+
+env.reset()
+for _ in range(5):
+    state, reward, done, info = env.step(action=0)
+    if done:
+        print("ASSET FAILED")
+        print(info["probability_failure"])
+        break
+
+    else:
+        print(info)
+        print(state)
+        print(reward, done)
+        print("")
 
